a90cb00f23dc064e938da0a94a6d515aad5bfc37
emileypalmquist <emiley.palmquist@icloud.com>
2020-10-29 16:48:18 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,7 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
-  end
-
-  def index
-    @posts = Post.all
+    @comment = Comment.new
   end
 
   def new
